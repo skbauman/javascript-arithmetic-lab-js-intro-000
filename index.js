e62b020c9bd25a1c01cd57259cecd4ca225b3b1e
Steve Bauman <skbauman@sbcglobal.net>
2018-06-05 17:13:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ function add(a, b) {
   var total = a + b;
   return total
 }
+function subtract(a, b) {
+  var total = a - b;
+  return total
+}
+function multiply(a, b) {
+  var total = a * b;
+  return total
+}
+function divide(a, b) {
+  var total = a / b;
+  return total
+}
